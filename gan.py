fix normalization bug

gan.py
@@ -1,3 +1,4 @@
+from torch.autograd import Variable
 import torch.nn as nn
 import torch
 
@@ -8,4 +9,42 @@ class GAN(nn.Module):
         # Ensure the highest iteration of pretrained model
         self.getPretrainedHighestIter()
 
-    
+    def getPretrainedHighestIter(self, model_folder = './model'):
+        import glob
+        import os
+        prev_model_name_list = glob.glob(os.path.join(model_folder, '*.pth.tar'))
+        self.max_iter = 0
+        for name in prev_model_name_list:
+            postfix_iter = int(name.split('_')[-1].split('.')[0])
+            if self.max_iter < postfix_iter:
+                self.max_iter = postfix_iter
+
+    def prepareBatchData(self, batch_real, batch_wait):
+        # batch_real = batch_real.transpose(2, 3).transpose(1, 2).float()
+        # batch_wait = batch_wait.transpose(2, 3).transpose(1, 2).float()
+        batch_real = Variable(batch_real).cuda()
+        batch_wait = Variable(batch_wait).cuda()
+        return batch_real, batch_wait
+
+    def saveModel(self, model, iteration, model_folder = './model', model_name = 'generator'):
+        """
+            Model name: wait_iter_100.pth.tar
+        """
+        import glob
+        import os
+        if not os.path.exists(model_folder):
+            os.mkdir(model_folder)      
+
+        # Save
+        model_iter = self.max_iter + iteration
+        model_name = 'wait_iter_' + model_name + '_' + str(model_iter) + '.pth.tar'
+        torch.save(model.state_dict(), os.path.join(model_folder, model_name))
+
+    def loadModel(self, model, model_folder = './model', model_name = 'generator'):
+        import glob
+        import os
+        model_name = 'wait_iter_' + model_name + '_' + str(self.max_iter) + '.pth.tar'
+        model_path = os.path.join(model_folder + model_name)
+        if os.path.exists(model_path):
+            model.load_state_dict(torch.load(model_path)).cuda()
+        return model

model.py
@@ -1,9 +1,11 @@
+import torchvision_extend.transforms as my_transforms
+from module import Generator, Discriminator
 from visualize import saveTransformResult
 from torch.autograd import Variable
-from module import Generator, Discriminator
 from torch.optim import Adam
 from gan import GAN
 import torch.nn as nn
+import numpy as np
 import torch
 
 class CustomCycleGAN(GAN):
@@ -11,7 +13,7 @@ class CustomCycleGAN(GAN):
     INDEX_wait_to_real = 0
     INDEX_real_to_wait = 1
 
-    def __init__(self, input_channel = 3, base_filter = 16, adopt_custom = False):
+    def __init__(self, input_channel = 3, base_filter = 2, adopt_custom = False):
         super(CustomCycleGAN, self).__init__()
         self.adopt_custom = adopt_custom
         print('-' * 20 + ' Build generator ' + '-' * 20)
@@ -50,11 +52,13 @@ class CustomCycleGAN(GAN):
             exit()
 
     def train(self, loader, epoch=1, verbose_period=5):
-        self.loadModel()
+        # Load pretrained model
+        self.load()
+        
         for i in range(epoch):
             for j, (batch_real_img, batch_wait_img) in enumerate(loader):
-                batch_real_img = Variable(batch_real_img).cuda()
-                batch_wait_img = Variable(batch_wait_img).cuda()
+                # saveTransformResult('./output', str(i) + '_' + str(j) + '.png', Variable(batch_real_img), Variable(torch.zeros_like(batch_real_img)))                
+                batch_real_img, batch_wait_img = self.prepareBatchData(batch_real_img, batch_wait_img)
 
                 # ------------------------------------------------------------------------
                 # 1st cycle 
@@ -72,8 +76,8 @@ class CustomCycleGAN(GAN):
 
                 self.discriminator_loss.backward(retain_graph=True)
                 self.generator_loss.backward()
-                discriminator_loss_1st = self.discriminator_loss
-                generator_loss_1st = self.generator_loss
+                discriminator_loss_1st = self.discriminator_loss.data.cpu().numpy()[0]
+                generator_loss_1st = self.generator_loss.data.cpu().numpy()[0]
 
                 self.wait_to_real_generator_optimizer.step()
                 self.real_to_wait_generator_optimizer.step()
@@ -95,8 +99,8 @@ class CustomCycleGAN(GAN):
 
                 self.discriminator_loss.backward(retain_graph=True)
                 self.generator_loss.backward()
-                discriminator_loss_2nd = self.discriminator_loss
-                generator_loss_2nd = self.generator_loss
+                discriminator_loss_2nd = self.discriminator_loss.data.cpu().numpy()[0]
+                generator_loss_2nd = self.generator_loss.data.cpu().numpy()[0]
 
                 self.real_to_wait_generator_optimizer.step()
                 self.wait_to_real_generator_optimizer.step()
@@ -107,9 +111,30 @@ class CustomCycleGAN(GAN):
                 # ------------------------------------------------------------------------
                 if j % verbose_period == 0:
                     print('epoch: ', i, '\titer: ', j, 
-                    '\t< 1st cycle >\tgen loss: ', self.generator_loss.data.cpu().numpy()[0], 
-                    '\tdis loss: ', self.discriminator_loss.data.cpu().numpy()[0],
-                    '\t< 2nd cycle >\tgen loss: ', self.generator_loss.data.cpu().numpy()[0], 
-                    '\tdis loss: ', self.discriminator_loss.data.cpu().numpy()[0])
+                        '\t< 1st cycle >\tgen loss: ', generator_loss_1st, '\tdis loss: ', discriminator_loss_1st,
+                        '\t< 2nd cycle >\tgen loss: ', generator_loss_2nd, '\tdis loss: ', discriminator_loss_2nd
+                    )
                     saveTransformResult('./output', str(i) + '_' + str(j) + '.png', batch_real_img, restore_img)
-                    self.saveModel(i * loader.iter_num + j)
+                    self.save(i * loader.iter_num + j)
+
+    def load(self):
+        self.real_to_wait_generator = self.loadModel(self.real_to_wait_generator, model_name = 'real_to_wait_generator')
+        self.wait_to_real_generator = self.loadModel(self.wait_to_real_generator, model_name = 'wait_to_real_generator')
+        self.real_discriminator = self.loadModel(self.real_discriminator, model_name = 'real_discriminator')
+        self.wait_discriminator = self.loadModel(self.wait_discriminator, model_name = 'wait_discriminator')
+
+    def save(self, idx, model_folder = './model'):
+        import glob
+        import os
+
+        # Remove all previous model
+        prev_model_name_list = glob.glob(os.path.join(model_folder, '*.pth.tar'))
+        for name in prev_model_name_list:
+            os.remove(name)
+        
+        # Save
+        self.saveModel(self.real_to_wait_generator, idx, model_name = 'real_to_wait_generator')
+        self.saveModel(self.wait_to_real_generator, idx, model_name = 'wait_to_real_generator')
+        self.saveModel(self.real_discriminator, idx, model_name = 'real_discriminator')
+        self.saveModel(self.wait_discriminator, idx, model_name = 'wait_discriminator')
+        

test.py
@@ -3,6 +3,8 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 import torchvision_extend.transforms as my_transforms
 import torchvision_extend.data as Data
+import numpy as np
+import torch
 import cv2
 
 dataset = Data.WaitTensorDataset(
@@ -10,15 +12,24 @@ dataset = Data.WaitTensorDataset(
     wait_img_root_dir = './wait/', 
     transform = transforms.Compose([
         my_transforms.Rescale((160, 320)),
-        my_transforms.ToTensor()
+        my_transforms.ToTensor(),
+        my_transforms.Normalize([127.5, 127.5, 127.5], [127.5, 127.5, 127.5])
     ])
 )
 loader = Data.WaitDataLoader(dataset, batch_size=32, shuffle=True)
-print(loader.iter_num)
 
 for batch_real_tensor, batch_wait_tensor in loader:
-    real_img = batch_real_tensor.numpy()[0]
-    wait_img = batch_wait_tensor.numpy()[0]
+    # Reverse normalization
+    unNorm = my_transforms.UnNormalize([127.5, 127.5, 127.5], [127.5, 127.5, 127.5])
+    batch_real_tensor = unNorm(batch_real_tensor)
+    batch_wait_tensor = unNorm(batch_wait_tensor)
+
+    print(torch.max(batch_real_tensor))
+    
+    # Transfer into HWC format before we show
+    real_img = batch_real_tensor.transpose(1, 2).transpose(2, 3).numpy()[0].astype(np.uint8)
+    wait_img = batch_wait_tensor.transpose(1, 2).transpose(2, 3).numpy()[0].astype(np.uint8)
+
     cv2.imshow('real', real_img)
     cv2.imshow('wait', wait_img)
     cv2.waitKey()

torchvision_extend/transforms.py
@@ -1,3 +1,4 @@
+import torchvision.transforms.functional as F
 import torch
 import cv2
 
@@ -23,7 +24,55 @@ class Rescale(object):
 class ToTensor(object):
     def __call__(self, sample):
         real_img, wait_img = sample['real'], sample['wait']
-        return {
-            'real': torch.from_numpy(real_img),
-            'wait': torch.from_numpy(wait_img)
-        }
+        real_img = torch.from_numpy(real_img)
+        wait_img = torch.from_numpy(wait_img)
+        real_img = real_img.transpose(1, 2).transpose(0, 1)
+        wait_img = wait_img.transpose(1, 2).transpose(0, 1)
+        return {'real': real_img, 'wait': wait_img}
+
+class Normalize(object):
+    """
+        Normalize toward two tensor
+    """
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, sample):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized Tensor image.
+        """
+        real_img, wait_img = sample['real'], sample['wait']
+        real_img = real_img.float() if type(real_img) == torch.ByteTensor else real_img
+        wait_img = wait_img.float() if type(wait_img) == torch.ByteTensor else wait_img       
+        real_img = F.normalize(real_img, self.mean, self.std)
+        wait_img = F.normalize(wait_img, self.mean, self.std)
+        return {'real': real_img, 'wait': wait_img}
+
+class UnNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized image.
+        """
+        tensor = tensor.transpose(0, 1)
+        # for slice_tensor in tensor:
+        result_tensor = []
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t = torch.mul(t, s)
+            t = t.add_(m)
+            result_tensor.append(t)
+            # The normalize code -> t.sub_(m).div_(s)
+        tensor = torch.stack(result_tensor, 0)
+        tensor = tensor.transpose(0, 1)
+        print(tensor.size())
+        return tensor

train.py
@@ -1,57 +1,25 @@
 import _init_paths
-import torchvision_extend.data as mData
 from torch.utils import data as Data
+from torch.autograd import Variable
+from torchvision import transforms
 from model import CustomCycleGAN
+import torchvision_extend.transforms as my_transforms
+import torchvision_extend.data as mData
 import numpy as np
 import torch
 
-def getPretrainedHighestIter(model_folder = './model'):
-        import glob
-        import os
-        prev_model_name_list = glob.glob(os.path.join(model_folder, '*.pth.tar'))
-        max_iter = 0
-        for name in prev_model_name_list:
-            postfix_iter = int(name.split('_')[-1].split('.')[0])
-            if max_iter < postfix_iter:
-                max_iter = postfix_iter
-        return max_iter
-
-def saveModel(self, model, iteration, model_folder = './model'):
-    """
-        Model name: wait_iter_100.pth.tar
-    """
-    import glob
-    import os
-    if not os.path.exists(model_folder):
-        os.mkdir(model_folder)      
-
-    # Remove all previous model
-    prev_model_name_list = glob.glob(os.path.join(model_folder, '*.pth.tar'))
-    for name in prev_model_name_list:
-        os.remove(name)
-
-    # Save
-    model_iter = self.max_iter + iteration
-    model_name = 'wait_iter_' + str(model_iter) + '.pth.tar'
-    torch.save(self.state_dict(), os.path.join(model_folder, model_name))
-
-def loadModel(self, model_folder = './model'):
-    import glob
-    import os
-    model_name = 'wait_iter_' + str(self.max_iter) + '.pth.tar'
-    model_path = os.path.join(model_folder + model_name)
-    if os.path.exists(model_path):
-        torch.load(model_path)
-
 if __name__ == '__main__':
     # Generate data
-    real_img = np.random.random([2000, 3, 160, 320])
-    wait_img = np.random.random([2000, 3, 160, 320])
-    dataset = Data.TensorDataset(
-        data_tensor = torch.from_numpy(real_img).float(),
-        target_tensor = torch.from_numpy(wait_img).float()
+    dataset = mData.WaitTensorDataset(
+        real_img_root_dir = './train2014/', 
+        wait_img_root_dir = './wait/', 
+        transform = transforms.Compose([
+                my_transforms.Rescale((160, 320)),
+                my_transforms.ToTensor(),
+                my_transforms.Normalize([127.5, 127.5, 127.5], [127.5, 127.5, 127.5])
+            ])
     )
-    loader = mData.WaitDataLoader(dataset = dataset, batch_size=32, shuffle=True)
+    loader = mData.WaitDataLoader(dataset = dataset, batch_size=32, shuffle=True, num_workers = 2)
 
     # Train
     model = CustomCycleGAN()

visualize.py
@@ -1,3 +1,4 @@
+import torchvision_extend.transforms as my_transforms
 import numpy as np
 import torch
 import cv2
@@ -7,6 +8,15 @@ def saveTransformResult(output_dir, img_name, real_variable, wait_vabiable):
     """
         Visualize for 4 image
     """
+    # Reverse normalization
+    # print("before: ", torch.max(real_variable))
+
+    unNorm = my_transforms.UnNormalize([127.5, 127.5, 127.5], [127.5, 127.5, 127.5])
+    real_variable = unNorm(real_variable)
+    wait_vabiable = unNorm(wait_vabiable)
+
+    # print("after: ", torch.max(real_variable))
+
     # BCHW -> BHWC
     real_variable = real_variable.transpose(1, 2).transpose(2, 3)
     wait_vabiable = wait_vabiable.transpose(1, 2).transpose(2, 3)
@@ -20,10 +30,11 @@ def saveTransformResult(output_dir, img_name, real_variable, wait_vabiable):
     for idx in range(min(4, real_variable.size(0))):
         real_img = real_variable.data.cpu().numpy()[idx]
         wait_img = wait_vabiable.data.cpu().numpy()[idx]
+
         column = np.concatenate((real_img, wait_img), axis=0)
         if result_img is None:
             result_img = column
         else:
             result_img = np.concatenate((result_img, column), axis=1)
-    result_img = (result_img * 255).astype(np.uint8)
+    result_img = (result_img).astype(np.uint8)
     cv2.imwrite(os.path.join(output_dir, img_name), result_img)
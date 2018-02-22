77560298478cdb1f216039c858759eb5d2a1a402
SunnerLi <a6214123@gmail.com>
2018-02-22 22:52:04 +0800

    remove missing file

--- redundant/test3.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import torch
-
-mask = torch.from_numpy(np.asarray([[0, 0], [1, 0]])).float()
-img = torch.from_numpy(np.asarray([[0.5, 0.6], [0.7, 0.8]])).float()
-mask = torch.ones_like(mask) - mask
-img = torch.mul(mask, img)
-print(img)

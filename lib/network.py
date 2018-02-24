1. fix normalize bug. range is [-1, 1] 2. change as instance normalization 3. add another normalize example in torchvision_sunner folder 4. ensure the training can work

lib/network.py
@@ -32,7 +32,7 @@ def print_network(net):
     print('Total number of parameters: %d' % num_params)
 
 def define_G(input_nc, output_nc, ngf, use_dropout=False, use_gpu = True):
-    norm_layer = functools.partial(nn.BatchNorm2d, affine=True)
+    norm_layer = functools.partial(nn.InstanceNorm2d, affine=False)
     netG = ResnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=6, use_gpu = use_gpu)
     if use_gpu:
         netG.cuda()
@@ -40,7 +40,7 @@ def define_G(input_nc, output_nc, ngf, use_dropout=False, use_gpu = True):
     return netG
 
 def define_D(input_nc, ndf, n_layers_D=3, use_sigmoid=False, use_gpu = True):
-    norm_layer = functools.partial(nn.BatchNorm2d, affine=True)
+    norm_layer = functools.partial(nn.InstanceNorm2d, affine=False)
     netD = NLayerDiscriminator(input_nc, ndf, n_layers=3, norm_layer=norm_layer, use_sigmoid=use_sigmoid, use_gpu = use_gpu)
     if use_gpu:
         netD.cuda()

photo_transfer.py
@@ -17,9 +17,12 @@ def transferImage(img, model_path = './model/', output_folder = './', result_img
     # Make as variable and normalized
     img = torch.from_numpy(img).float()
     img = img.transpose(1, 2).transpose(0, 1)
-    mean_list = [127.5, 127.5, 127.5]
-    std_list = [127.5, 127.5, 127.5]
-    img = F.normalize(img, mean_list, std_list)
+
+    # Normalize
+    for t in img:
+        t = torch.div(t, 255).mul_(2).add_(-1)
+
+    # Form the input tensor
     img = img.numpy()[np.newaxis, :]
     img = torch.from_numpy(img).float()
 